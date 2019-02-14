eb9fbaaadcc5188074f1182621d5f27e110f023a
yoichi <yy.motoyama@gmail.com>
2019-02-14 21:28:16 +0900

    cretate Xcode project

---
+++ CollectionViewSemiModalTransitioning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewSemiModalTransitioning
+//
+//  Created by Yoichi on 2019/02/12.
+//  Copyright © 2019 Yoichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
